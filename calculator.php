3652e59f3305370c9d5155b765daf6b65e4fdf68
S Murad <salmanm2003@hotmail.com>
2015-03-06 01:13:32 +0000

    the operation has been done for * and / - testing

--- calculator.php
+++ calculator.php
@@ -4,7 +4,7 @@
 class Calculator
 {
 
-	public function calculate($cal_input = '12+3')
+	public function calculate($cal_input = '3*4')
 	{
 
 		echo "<form name='cal' method='post' action='' >
@@ -17,27 +17,47 @@ class Calculator
 		if($cal_input != '')
 		{
 
-			preg_match('/^[0-9]*/', $cal_input, $matches);
-			$cal_rest  = preg_replace('/^[0-9]*/','',$cal_input);
-			print_r($matches);
+			preg_match('/^[0-9\.]*/', $cal_input, $matches);
+			$cal_rest  = preg_replace('/^[0-9\.]*/','',$cal_input);
+			echo 'first:';
+			echo $first_num = $matches[0];
 			
 			$op_arr = ['*','/'];
 			
-			if(in_array($cal_rest[0],$op_arr)
+			if(in_array($cal_rest[0],$op_arr))
 			{
 			
-				if(cal_rest[0] == '*')
+				if($cal_rest[0] == '*')
 				{
 					
-					$cal_rest = substr(cal_rest,1);
+					echo 'op=';echo $cal_rest[0];
+					echo $cal_rest = substr($cal_rest,1);
 					
-					preg_match('/^[0-9]*/', $cal_rest, $matches_rest);
+					preg_match('/^[0-9\.]*/', $cal_rest, $matches_rest);
 					
-					$cal_rest = preg_replace('/^[0-9]*/','',$cal_rest);
-					$cal_rest = (intval)$matches * (intval)$matches_rest;
+					echo 'sec';
+					echo $sec_num = $matches_rest[0];
+					echo '<br/>';
+					echo $cal_rest = preg_replace('/^[0-9\.]*/','',$cal_rest);
+					echo 'result';
+					echo $cal_sub  = floatval($first_num) * floatval($sec_num);
+					
+				}
+				elseif($cal_rest[0] == '/')
+				{
+					
+					echo $cal_rest = substr($cal_rest,1);
+					
+					preg_match('/^[0-9\.]*/', $cal_rest, $matches_rest);
+					
+					$cal_rest = preg_replace('/^[0-9\.]*/','',$cal_rest);
+					$cal_sub  = floatval($matches) / floatval($matches_rest);
 					
 				}
 				   
+				echo $cal_rest = $cal_sub.$cal_rest;
+				
+			
 			}
 			   
 			
@@ -57,6 +77,3 @@ class Calculator
 
 $cal = new Calculator();
 $cal->calculate();
-
-
-?>
